f04150a621e1167fb992b213eae99872b244eafc
Somesh Lingwal <someshlingwal1@gmail.com>
2026-04-06 14:19:11 +0530

    Address updation

--- visa-rapid/src/components/Footer.tsx
+++ visa-rapid/src/components/Footer.tsx
@@ -111,6 +111,13 @@ const Footer = () => {
                 </svg>
                 <span>info@visarapid.com</span>
               </div>
+              <div className="flex items-start text-gray-300">
+                <svg className="w-5 h-5 mr-3 mt-1 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+                <span>Head office- Spaces Matosinhos, Avenida Meneres 640, Matosinhos 4450-010, Portugal</span>
+              </div>
             </div>
 
             <div className="mt-8">
